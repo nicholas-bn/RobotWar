0d5a9e5a92050b68ff395a244e16ddba078f9fcc
Karl Ferrero <karl.ferrero.kf@gmail.com>
2016-10-24 16:20:18 +0200

    #1 Création de la classe Grille
    Test de la classe en créant des jlabels de couleurs différentes.

---
+++ src/gui/Grille.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ * Classe représentant une grille de jeu
+ * 
+ * @author Karl
+ *
+ */
+@SuppressWarnings("serial")
+public class Grille extends JLabel {
+
+	/** Nombre max de cases par colonnes */
+	private static final int nbColonnes = 10;
+	/** Nombre max de cases par lignes */
+	private static final int nbLignes = 10;
+
+	/**
+	 * Constructeur de la classe {@link Grille}
+	 */
+	public Grille() {
+		super();
+		// Layout utilisé
+		setLayout(new GridLayout(10, 10));
+
+		// Construction de la grille :
+		for (int row = 0; row < nbLignes; row++) {
+			for (int col = 0; col < nbColonnes; col++) {
+				// Création d'une case TODO remplacer par la case CaseGrille
+				Random rand = new Random();
+				float r = rand.nextFloat();
+				float g = rand.nextFloat();
+				float b = rand.nextFloat();
+				Color randomColor = new Color(r, g, b);
+
+				JLabel label = new JLabel();
+				label.setOpaque(true);
+				label.setBackground(randomColor);
+
+				// Ajout de la case à la grille
+				add(label, row, col);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Test de la classe Grille");
+		Grille grille = new Grille();
+		frame.setContentPane(grille);
+
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
