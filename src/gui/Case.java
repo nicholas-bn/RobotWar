0845171c5ad582fb61f31d0c93a3ec53bd905f6e
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-10-24 19:06:50 +0200

    #2 Ajout d'une barre de vie suivant un pourcentage donné

--- src/gui/Case.java
+++ src/gui/Case.java
@@ -1,7 +1,9 @@
 package gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -54,6 +56,34 @@ public class Case extends JLabel {
 		
 		// Dessine au milieu du JLabel
 		g.fillOval((xLabel - xDraw)/2, (yLabel - yDraw)/2, xDraw, yDraw);
+		
+		// Dessiner le background de la barre de vie
+		g.setColor(Color.white);
+		Graphics2D g2 = (Graphics2D) g;
+	    g2.setStroke(new BasicStroke(3));
+	    int x1 = xLabel/8;
+	    int y1 = yLabel/8;
+	    int x2 = (xLabel*7)/8;
+	    int y2 = yLabel/8;
+		g.drawLine(x1, y1, x2, y2);
+		System.out.println("TEST1");
+		System.out.println(x1+" "+ y1+" "+ x2+" "+ y2);
+		
+		// Dessiner la barre de vie
+		
+		float vie = 0.50f; // A ENLEVER PLUS TARD: on prendra la vie du robot
+		float tailleLigne = (xLabel - (xLabel/4)) * vie;
+		float positionFinLigneVerte = (xLabel/8) + tailleLigne;
+		
+		g.setColor(Color.green);
+	    g2.setStroke(new BasicStroke(3));
+	    x1 = xLabel/8;
+	    y1 = yLabel/8;
+	    x2 = (int) positionFinLigneVerte;
+	    y2 = yLabel/8;
+		g.drawLine(x1, y1, x2, y2);
+		System.out.println("TEST2");
+		System.out.println(x1+" "+ y1+" "+ x2+" "+ y2);
 
 	}
 
@@ -62,8 +92,6 @@ public class Case extends JLabel {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setSize(100, 100);
 		jf.setLocation(100, 100);
-		//test commit
-		System.out.println("madaaaaame");
 
 		Case cg = new Case();
 
