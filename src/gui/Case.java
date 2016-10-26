b2f824de9220f5c4a09813bdfb6fa2f531d9e887
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-10-26 14:59:06 +0200

    #4 Chargement dynamique de la classe Test_Dynamic_Loading.java
    On a pu utiliser cette classe et sa méthode paint depuis Case.java

--- src/gui/Case.java
+++ src/gui/Case.java
@@ -1,13 +1,15 @@
 package gui;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import plugins.Test_Dynamic_Loading;
 
 /**
  * Classe représentant une case de la grille
@@ -39,47 +41,78 @@ public class Case extends JLabel {
 
 	
 	/**
-	 * Méthode paintComponent de {@link Case}
+	 * Méthode paint de {@link Case}
 	 */
 	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.setColor(Color.black);
-
-		// Taille du label
-		int xLabel = this.getSize().width;
-		int yLabel = this.getSize().height;
+	public void paint(Graphics g) {
+		super.paint(g);
 		
-		// Centre du label
-		int xDraw = xLabel / 2 ;
-		int yDraw = yLabel / 2 ;
+		try {
+			Class c = Class.forName("plugins.Test_Dynamic_Loading");
+			
+			Test_Dynamic_Loading instance = (Test_Dynamic_Loading) c.newInstance();
+			
+			Method m = c.getMethod("dessiner",int.class, int.class, Graphics.class);
+			m.invoke(instance, this.getWidth(), this.getHeight(), g);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		// Dessine au milieu du JLabel
-		g.fillOval((xLabel - xDraw)/2, (yLabel - yDraw)/2, xDraw, yDraw);
-		
-		// Dessiner le background de la barre de vie
-		g.setColor(Color.white);
-		Graphics2D g2 = (Graphics2D) g;
-	    g2.setStroke(new BasicStroke(3));
-	    int x1 = xLabel/8;
-	    int y1 = yLabel/8;
-	    int x2 = (xLabel*7)/8;
-	    int y2 = yLabel/8;
-		g.drawLine(x1, y1, x2, y2);
-		
-		// Dessiner la barre de vie
-		
-		float vie = 0.50f; // A ENLEVER PLUS TARD: on prendra la vie du robot
-		float tailleLigne = (xLabel - (xLabel/4)) * vie;
-		float positionFinLigneVerte = (xLabel/8) + tailleLigne;
-		
-		g.setColor(Color.green);
-	    g2.setStroke(new BasicStroke(3));
-	    x1 = xLabel/8;
-	    y1 = yLabel/8;
-	    x2 = (int) positionFinLigneVerte;
-	    y2 = yLabel/8;
-		g.drawLine(x1, y1, x2, y2);
+//		g.setColor(Color.black);
+//
+//		// Taille du label
+//		int xLabel = this.getSize().width;
+//		int yLabel = this.getSize().height;
+//		
+//		// Centre du label
+//		int xDraw = xLabel / 2 ;
+//		int yDraw = yLabel / 2 ;
+//		
+//		// Dessine au milieu du JLabel
+//		g.fillOval((xLabel - xDraw)/2, (yLabel - yDraw)/2, xDraw, yDraw);
+//		
+//		// Dessiner le background de la barre de vie
+//		g.setColor(Color.white);
+//		Graphics2D g2 = (Graphics2D) g;
+//	    g2.setStroke(new BasicStroke(3));
+//	    int x1 = xLabel/8;
+//	    int y1 = yLabel/8;
+//	    int x2 = (xLabel*7)/8;
+//	    int y2 = yLabel/8;
+//		g.drawLine(x1, y1, x2, y2);
+//		
+//		// Dessiner la barre de vie
+//		
+//		float vie = 0.50f; // A ENLEVER PLUS TARD: on prendra la vie du robot
+//		float tailleLigne = (xLabel - (xLabel/4)) * vie;
+//		float positionFinLigneVerte = (xLabel/8) + tailleLigne;
+//		
+//		g.setColor(Color.green);
+//	    g2.setStroke(new BasicStroke(3));
+//	    x1 = xLabel/8;
+//	    y1 = yLabel/8;
+//	    x2 = (int) positionFinLigneVerte;
+//	    y2 = yLabel/8;
+//		g.drawLine(x1, y1, x2, y2);
 
 	}
 
---
+++ src/plugins/Test_Dynamic_Loading.java
@@ -0,0 +1,70 @@
+package plugins;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import gui.Case;
+
+public class Test_Dynamic_Loading extends Component{
+	
+	int width;
+	int height;
+	
+	
+	public void dessiner(int width, int height, Graphics g){
+		System.out.println("DESSSSSSSSSSSSSSSSSINER");
+		this.width=width;
+		this.height=height;
+		paint(g);
+	}
+	
+	/**
+	 * Méthode paintComponent de {@link Case}
+	 */
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+		System.out.println("waddup");
+		g.setColor(Color.black);
+
+		// Taille du label
+		int xLabel = this.width;
+		int yLabel = this.height;
+
+		// Centre du label
+		int xDraw = xLabel / 2;
+		int yDraw = yLabel / 2;
+
+		// Dessine au milieu du JLabel
+		g.fillOval((xLabel - xDraw) / 2, (yLabel - yDraw) / 2, xDraw, yDraw);
+
+		// Dessiner le background de la barre de vie
+		g.setColor(Color.white);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(3));
+		int x1 = xLabel / 8;
+		int y1 = yLabel / 8;
+		int x2 = (xLabel * 7) / 8;
+		int y2 = yLabel / 8;
+		g.drawLine(x1, y1, x2, y2);
+
+		// Dessiner la barre de vie
+
+		float vie = 0.50f; // A ENLEVER PLUS TARD: on prendra la vie du robot
+		float tailleLigne = (xLabel - (xLabel / 4)) * vie;
+		float positionFinLigneVerte = (xLabel / 8) + tailleLigne;
+
+		g.setColor(Color.green);
+		g2.setStroke(new BasicStroke(3));
+		x1 = xLabel / 8;
+		y1 = yLabel / 8;
+		x2 = (int) positionFinLigneVerte;
+		y2 = yLabel / 8;
+		g.drawLine(x1, y1, x2, y2);
+
+	}
+}
