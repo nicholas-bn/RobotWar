ab0d70c4d7700b7d41e330902c599843618a2589
Léonard Deï <ldei0066@gmail.com>
2016-10-27 15:44:18 +0200

    #10 Modification

--- src/gui/Moteur.java
+++ src/gui/Moteur.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
  *
  */
 public class Moteur {
-	
+
 	ArrayList<Robot> list = new ArrayList<>();
 
 	Grille grille;
@@ -23,35 +23,18 @@ public class Moteur {
 
 		// Instantiation de la grille de jeu
 		creationDeLaGrilleDeJeu();
-		
-		
-		for (int k=0; k < i; k++){
-			list.add(robot);
-			int randomx = (int) (Math.random() * (Grille.getNbcolonnesmax() - 0));
-			int randomy = (int) (Math.random() * (Grille.getNblignesmax() - 0));
-			Point point = new Point(randomx, randomy);
+
+		//Création des robots
+		for (int k = 0; k < i; k++) {
+			robot = new Robot();
+			int randomX = (int) (Math.random() * (Grille.getNbcolonnesmax() - 0));
+			int randomY = (int) (Math.random() * (Grille.getNblignesmax() - 0));
+			Point point = new Point(randomX, randomY);
 			robot.setPoint(point);
 			robot.setPtMouvement(1);
-			grille.getElementsGrille()[randomx][randomy].setRobot(robot);
+			grille.getElementsGrille()[randomX][randomY].setRobot(robot);
+			list.add(robot);
 		}
-
-//		// Création du premier robot
-//		r = new Robot();
-//		int randomx = (int) (Math.random() * (Grille.getNbcolonnesmax() - 0));
-//		int randomy = (int) (Math.random() * (Grille.getNblignesmax() - 0));
-//		Point point = new Point(randomx, randomy);
-//		r.setPoint(point);
-//		r.setPtMouvement(1);
-//		grille.getElementsGrille()[randomx][randomy].setRobot(r);
-//
-//		// Création du deuxième robot
-//		r2 = new Robot();
-//		randomx = (int) (Math.random() * (Grille.getNbcolonnesmax() - 0));
-//		randomy = (int) (Math.random() * (Grille.getNblignesmax() - 0));
-//		point = new Point(randomx, randomy);
-//		r2.setPoint(point);
-//		r2.setPtMouvement(1);
-//		grille.getElementsGrille()[randomx][randomy].setRobot(r2);
 
 		// Démarrage des tours
 		gestionDesTours();
@@ -76,27 +59,25 @@ public class Moteur {
 	 */
 	public void gestionDesTours() {
 		
+		
+		// Boucle pour chaque tour de jeu :
+		for (int i = 0; i < 10; i++) {
+		try {
+			System.out.println("Tour : "+(i+1));
+			TimeUnit.SECONDS.sleep(1); }
+		 catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		for (Robot robot : list){
+			// On demande au robot de se déplacer
 			robot.seDeplacer(grille);
+		} 
 		}
 
-//		// Boucle pour chaque tour de jeu :
-//		for (int i = 0; i < 10; i++) {
-//			try {
-//				TimeUnit.SECONDS.sleep(1);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//			// On demande au robot de se déplacer
-//			r.seDeplacer(grille);
-//
-//			r2.seDeplacer(grille);
-//
-//		}
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 
 		Moteur m = new Moteur(5);
 
