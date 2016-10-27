9a330fde578ed15473d82846183e225c11cbe55d
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-10-27 15:33:45 +0200

    #11 Création de la classe

---
+++ src/plugins/Attaque_de_Base.java
@@ -0,0 +1,43 @@
+package plugins;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
+
+import gui.Grille;
+import gui.Robot;
+
+/**
+ * Classe représentant une case de la grille
+ * 
+ * @author Barnini Nicholas
+ *
+ */
+
+public class Attaque_de_Base {
+
+	/**
+	 * Choisit une cible
+	 * 
+	 * @param grille
+	 * @param robot
+	 * @return Robot
+	 */
+	public Robot choisirCible(Grille grille, Robot robot) {
+		// Position actuelle du robot
+		return null;
+
+	}
+	
+	/**
+	 * Retourne la liste des robots dans la portée du robot attaquant <br>
+	 * 
+	 * @param x
+	 * @param y
+	 * @return ArrayList<Robot>
+	 */
+	
+	public ArrayList<Robot> getListeAttaquesPossibles(int x, int y, int pm) {
+		return null;
+	}
+}
