fb987c644f6f5d9b4b56e182a38f4a54da8fdbd1
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-10-27 21:09:48 +0200

    #18 Ajout d'un TU dans le cas ou il y a plusieurs robots retenus
    On vérifie bien que le robot le plus faible est choisi à attaquer

--- test/plugins/Test_plugin_attaque_de_base.java
+++ test/plugins/Test_plugin_attaque_de_base.java
@@ -1,6 +1,6 @@
 package plugins;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.awt.Point;
 
@@ -14,6 +14,7 @@ public class Test_plugin_attaque_de_base {
 
 	Attaque_de_Base attaque;
 	Robot robotAttaquant;
+	Robot robotVictimeFaible;
 	Robot robotVictime;
 	Grille grille;
 	
@@ -21,6 +22,7 @@ public class Test_plugin_attaque_de_base {
 	public void test() {
 		attaque = new Attaque_de_Base();
 		robotAttaquant = new Robot();
+		robotVictimeFaible = new Robot();
 		robotVictime = new Robot();
 		grille = new Grille();
 	}
@@ -39,7 +41,7 @@ public class Test_plugin_attaque_de_base {
 		grille.getElementsGrille()[3][3].setRobot(robotVictime);
 		
 		// Le robot étant à la limite de la portée, il n'est pas pris en compte
-		assertTrue(attaque.choisirCible(grille, robotAttaquant).equals(null));
+		assertTrue(attaque.choisirCible(grille, robotAttaquant)==null);
 	}
 	
 	@Test
@@ -59,4 +61,25 @@ public class Test_plugin_attaque_de_base {
 		assertTrue(attaque.choisirCible(grille, robotAttaquant).equals(robotVictime));
 	}
 	
+	@Test
+	public void test_que_le_bon_robot_est_choisi_lorsque_il_y_a_plusieurs_robots(){
+
+		// Initialisation du robot attaquant
+		robotAttaquant.setPortee(4);
+		robotAttaquant.setPoint(new Point(0, 0));
+		grille.getElementsGrille()[0][0].setRobot(robotAttaquant);
+
+		// Initialisation du robot se faisant attaquer ayant le plus de vie
+		robotVictime.setVie(100);
+		robotVictime.setPoint(new Point(2, 2));
+		grille.getElementsGrille()[2][2].setRobot(robotVictime);
+		
+		// Initialisation du robot se faisant attaquer ayant le moins de vie
+		robotVictimeFaible.setVie(90);
+		robotVictimeFaible.setPoint(new Point(3, 3));
+		grille.getElementsGrille()[3][3].setRobot(robotVictimeFaible);
+		
+		// Le robot étant le seul à être à portée est attaqué
+		assertTrue(attaque.choisirCible(grille, robotAttaquant).equals(robotVictimeFaible));
+	}
 }
