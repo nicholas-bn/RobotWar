a3502cffa72875378e0f6b32c1417a3fdcc1af3c
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-11-02 08:17:30 +0100

    #36 Ajout d'un cadre noir à la barre de vie

--- src/plugins/graphisme/Barre_de_vie.java
+++ src/plugins/graphisme/Barre_de_vie.java
@@ -26,13 +26,22 @@ public class Barre_de_vie extends Graphisme_de_Base {
 
 		Graphics2D g2 = (Graphics2D) g;
 
-		// Dessiner le background de la barre de vie
-		g.setColor(Color.white);
-		g2.setStroke(new BasicStroke(3));
+		// Dessiner l'arriere plan noir de la barre de vie
+		g.setColor(Color.black);
+		g2.setStroke(new BasicStroke(5));
 		int x1 = xLabel / 8;
 		int y1 = yLabel / 8;
 		int x2 = (xLabel * 7) / 8;
 		int y2 = yLabel / 8;
+		g.drawLine(x1, y1, x2, y2);
+		
+		// Dessiner le fond de la barre de vie
+		g.setColor(Color.white);
+		g2.setStroke(new BasicStroke(3));
+		x1 = xLabel / 8;
+		y1 = yLabel / 8;
+		x2 = (xLabel * 7) / 8;
+		y2 = yLabel / 8;
 		g.drawLine(x1, y1, x2, y2);
 
 		Robot robot = c.getRobot();
